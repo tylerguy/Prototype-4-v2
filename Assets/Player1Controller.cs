b6a892eb33c08c76ba6139cc70b9a90cf7d83327
tylerguy <tylerdenman200@gmail.com>
2023-03-23 16:04:33 +0000

    Update controller for player 1

--- Assets/Player1Controller.cs
+++ Assets/Player1Controller.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Player1Controller : MonoBehaviour
 {
+    public float speed = 10.0f;
+
+    public Camera cam;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
     }
 }
