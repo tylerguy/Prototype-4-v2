fb19461f80cf86fe453bd2628ce2c7a526c49f10
tylerguy <tylerdenman200@gmail.com>
2023-03-23 16:06:30 +0000

    Add player 2 controller

--- Assets/Player2Controller.cs
+++ Assets/Player2Controller.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class Player2Controller : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float speed = 10.0f;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
     }
 }
